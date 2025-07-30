2ab4c1cb98768a870d9c6d9d52020135bb8abc76
Jennifer Maia <jen_maia@hotmail.com>
2025-07-30 19:14:34 -0300

    update

--- exemplo_01.py
+++ exemplo_01.py
@@ -2,8 +2,8 @@ import requests
 
 from pydantic import BaseModel
 
-class PokemonSchema(BaseModel):
-    name: int
+class PokemonSchema(BaseModel): # Contrato de dados, schema de dados, a view da minha API
+    name: str
     type: str
 
     class Config:
@@ -12,7 +12,6 @@ class PokemonSchema(BaseModel):
 def pegar_pokemon(id: int) -> PokemonSchema:
     response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{id}")
     data = response.json()
-    print(data)
     data_types = data['types']  # Supondo que 'data' é o dicionário com os dados do Pokémon
     types_list = []
     for type_info in data_types:
@@ -20,7 +19,11 @@ def pegar_pokemon(id: int) -> PokemonSchema:
     types = ', '.join(types_list)
     return PokemonSchema(name=data['name'], type=types)
 
-from pydantic import BaseModel
+if __name__ == "__main__":
+    print(pegar_pokemon(10))
+    print(pegar_pokemon(25))
 
-pokemon = pegar_pokemon(24)
-print(pokemon)
+# from pydantic import BaseModel
+
+# pokemon = pegar_pokemon(24)
+# print(pokemon)
